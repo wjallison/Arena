ae2c1603480402a2b3c1719907904cde30af3996
wjallison_WorkComputer <38045135+wjallison@users.noreply.github.com>
2019-04-24 14:49:13 -0700

    Init

---
+++ Arena/ShipBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Arena
+{
+    public class ShipBlock
+    {
+        private double hp;
+        public double HP { get { return hp; } set { hp = value; } }
+        public double mass;
+        public List<double> damageResistances = new List<double>();
+
+        public ShipBlock() { }
+    }
+}
